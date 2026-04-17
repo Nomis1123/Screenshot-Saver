202315644923848704c254ffbdbb82051b103ca1
Simon Hunter <72155251+Nomis1123@users.noreply.github.com>
2026-04-17 11:26:43 -0400

    setting up main screenshot logic

--- main.py
+++ main.py
@@ -14,6 +14,28 @@ from pynput import keyboard
 SCREENSHOT_KEY = keyboard.Key.f9
 EXIT_KEY = keyboard.Key.f10
 
+SCREENSHOT_DIR = Path.home() / "screenshots"
+SCREENSHOT_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def screenshot_taker() -> bool:
+    try:
+        currentTime = datetime.now()
+        string_time = currentTime.strftime("%Y%m%d%H%M%S")
+        filePath = SCREENSHOT_DIR / f"{string_time}.png"
+
+        with mss() as sct:
+            monitor = sct.monitors[0]
+            data = sct.grab(monitor)
+
+            final_data = mss.tools.to_png(data.rgb, data.size, output=filePath)
+
+        print("Screenshot Saved to " + filePath)
+        return True
+
+    except Exception as e:
+        print(f"❌ Error taking screenshot: {e}")
+        return False
 
 
 def main():
